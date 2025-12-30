f781aee7bb400c9e6c26bae67d41d248fc6b7b8e
rupaya-dev <haldharsingh.epic@gmail.com>
2025-12-30 14:29:18 +0530

    Update clinics.js

--- clinics.js
+++ clinics.js
@@ -1,4 +1,31 @@
-const mongoose = require('mongoose');
+// models/clinic.js
+let mongoose;
+try {
+  mongoose = require('mongoose');
+} catch (error) {
+  // अगर mongoose नहीं installed है तो mock बनाएं
+  mongoose = {
+    Schema: class Schema {
+      constructor(obj) {
+        this.obj = obj;
+      }
+    },
+    model: function(name, schema) {
+      return class MockModel {
+        static find() {
+          return Promise.resolve([]);
+        }
+        static deleteMany() {
+          return Promise.resolve();
+        }
+        static insertMany() {
+          return Promise.resolve();
+        }
+      };
+    }
+  };
+  console.log('⚠️ Using mock mongoose (for development)');
+}
 
 const clinicSchema = new mongoose.Schema({
   name: String,
